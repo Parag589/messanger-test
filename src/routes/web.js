8b2c9c1f866b62d19227af044490f53d9335f596
Parag Agarwal <parag.a@w3dev.email>
2024-02-19 23:24:14 +0530

    deploy

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,17 @@
 import express from "express";
+import chatbotController from "../controllers/chatbotController";
+
 let router = express.Router();
 
 // const app = express()
 let initWebRoutes = (app) => {
 
-    router.get("/", (req,res) =>{
-        return res.send("Hello World")
-    })
-    // Home page route
+    router.get("/", chatbotController.test)
+    
+    router.get("/webhook", chatbotController.getWebHook)
+    router.post("/webhook", chatbotController.postWebHook)
+
+
     return app.use("/", router);
 }
 
